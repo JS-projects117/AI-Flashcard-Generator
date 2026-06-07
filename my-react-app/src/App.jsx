b96774184c6ec1626ba69538ab0ac679954ab121
JS-projects117 <josephsf221@gmail.com>
2026-06-07 11:40:27 -0700

    added json file

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import staticNames from "./staticNames.json"
+//import staticNames from "..staticNames.json"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
     <>
  <body>
   <header>
-    {staticNames}
+    d
   </header>
  </body>
     </>
